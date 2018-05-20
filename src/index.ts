bc704fee0fc397ef431f35b973c2bb21110db728
niuba <niuba.zbz@gmail.com>
2018-05-21 00:19:27 +0800

    update @anyres/core and rxjs
    Signed-off-by: niuba <niuba.zbz@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import {
   IAnyresResponse,
   IHttpAdapter,
 } from "@anyres/core";
-import "rxjs/add/observable/fromPromise";
-import "rxjs/add/operator/map";
-import { Observable } from "rxjs/Observable";
+import { from as observableFrom, Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import wepy from "wepy";
 
 export interface IWepyResponse {
@@ -28,21 +27,23 @@ export class WepyAdapter implements IHttpAdapter {
         params: {},
       };
     }
-    return Observable.fromPromise<IWepyResponse>(wepy.request({
+    return observableFrom<IWepyResponse>(wepy.request({
       url,
       data: options.params,
       header: options.headers,
       method: "GET",
       dataType: "json",
     }))
-      .map((response) => {
-        return {
-          status: response.statusCode,
-          headers: response.header,
-          body: response.data,
-          json: () => response.data,
-        };
-      });
+      .pipe(
+        map((response) => {
+          return {
+            status: response.statusCode,
+            headers: response.header,
+            body: response.data,
+            json: () => response.data,
+          };
+        }),
+    );
   }
 
   public post(url: string, options?: IAnyresRequestOptions): Observable<IAnyresResponse> {
@@ -52,21 +53,23 @@ export class WepyAdapter implements IHttpAdapter {
         params: {},
       };
     }
-    return Observable.fromPromise<IWepyResponse>(wepy.request({
+    return observableFrom<IWepyResponse>(wepy.request({
       url,
       data: options.body,
       header: options.headers,
       method: "POST",
       dataType: "json",
     }))
-      .map((response) => {
-        return {
-          status: response.statusCode,
-          headers: response.header,
-          body: response.data,
-          json: () => response.data,
-        };
-      });
+      .pipe(
+        map((response) => {
+          return {
+            status: response.statusCode,
+            headers: response.header,
+            body: response.data,
+            json: () => response.data,
+          };
+        }),
+    );
   }
 
   public put(url: string, options?: IAnyresRequestOptions): Observable<IAnyresResponse> {
@@ -76,21 +79,23 @@ export class WepyAdapter implements IHttpAdapter {
         params: {},
       };
     }
-    return Observable.fromPromise<IWepyResponse>(wepy.request({
+    return observableFrom<IWepyResponse>(wepy.request({
       url,
       data: options.body,
       header: options.headers,
       method: "PUT",
       dataType: "json",
     }))
-      .map((response) => {
-        return {
-          status: response.statusCode,
-          headers: response.header,
-          body: response.data,
-          json: () => response.data,
-        };
-      });
+      .pipe(
+        map((response) => {
+          return {
+            status: response.statusCode,
+            headers: response.header,
+            body: response.data,
+            json: () => response.data,
+          };
+        }),
+    );
   }
 
   public delete(url: string, options?: IAnyresRequestOptions): Observable<IAnyresResponse> {
@@ -100,20 +105,22 @@ export class WepyAdapter implements IHttpAdapter {
         params: {},
       };
     }
-    return Observable.fromPromise<IWepyResponse>(wepy.request({
+    return observableFrom<IWepyResponse>(wepy.request({
       url,
       header: options.headers,
       method: "DELETE",
       dataType: "json",
     }))
-      .map((response) => {
-        return {
-          status: response.statusCode,
-          headers: response.header,
-          body: response.data,
-          json: () => response.data,
-        };
-      });
+      .pipe(
+        map((response) => {
+          return {
+            status: response.statusCode,
+            headers: response.header,
+            body: response.data,
+            json: () => response.data,
+          };
+        }),
+    );
   }
 
   public patch(url: string, options?: IAnyresRequestOptions): Observable<IAnyresResponse> {
@@ -123,21 +130,23 @@ export class WepyAdapter implements IHttpAdapter {
         params: {},
       };
     }
-    return Observable.fromPromise<IWepyResponse>(wepy.request({
+    return observableFrom<IWepyResponse>(wepy.request({
       url,
       data: options.body,
       header: options.headers,
       method: "PUT",
       dataType: "json",
     }))
-      .map((response) => {
-        return {
-          status: response.statusCode,
-          headers: response.header,
-          body: response.data,
-          json: () => response.data,
-        };
-      });
+      .pipe(
+        map((response) => {
+          return {
+            status: response.statusCode,
+            headers: response.header,
+            body: response.data,
+            json: () => response.data,
+          };
+        }),
+    );
   }
 
 }
